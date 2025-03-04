8d85e886b9dae46b7129f88ff1a4c41618eaf0c4
DurlaVerder <vrrrr227@gmail.com>
2025-03-04 21:49:07 +0300

    Added table users and env var for auth-service

--- init.sql
+++ init.sql
@@ -1,2 +1,9 @@
 -- Table and index for Postgres --
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
